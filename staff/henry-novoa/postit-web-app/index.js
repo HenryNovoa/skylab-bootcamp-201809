3b4f233f2aba9da96f30d4d7f96058aef7ad6675
henry <henry.r.novoa@gmail.com>
2018-11-05 12:14:40 +0100

    weekend web-app

--- staff/henry-novoa/postit-web-app/index.js
+++ staff/henry-novoa/postit-web-app/index.js
@@ -1,26 +1,23 @@
 require('dotenv').config()
 const express = require('express')
 const session = require('express-session')
-// const FileStore = require('session-file-store')(session)
-const sessionFileStore = require('session-file-store')
-const FileStore = sessionFileStore(session)
+const FileStore = require('session-file-store')(session)
 const bodyParser = require('body-parser')
-const buildView = require('./helpers/build-view')
 const logic = require('./logic')
+const package = require('./package.json')
 
 const { argv: [, , port = process.env.PORT || 8080] } = process
 
 const app = express()
 
 app.use(express.static('./public'))
-
-let error = null
+app.set('view engine', 'pug')
 
 const formBodyParser = bodyParser.urlencoded({ extended: false })
 
 const mySession = session({
     secret: 'my super secret',
-    cookie: { maxAge: 60 * 60 * 24 },
+    cookie: { maxAge: 60 * 60 * 24 * 1000 },
     resave: true,
     saveUninitialized: true,
     store: new FileStore({
@@ -28,22 +25,16 @@ const mySession = session({
     })
 })
 
-app.get('/', (req, res) => {
-    error = null
+app.use(mySession)
 
-    res.send(buildView(`<a href="/login">Login</a> or <a href="/register">Register</a>`))
+app.get('/', (req, res) => {
+    req.session.error = null
+
+    res.render('landing')
 })
 
 app.get('/register', (req, res) => {
-    res.send(buildView(`<form action="/register" method="POST">
-            <input type="text" name="name" placeholder="Name">
-            <input type="text" name="surname" placeholder="Surname">
-            <input type="text" name="username" placeholder="username">
-            <input type="password" name="password" placeholder="password">
-            <button type="submit">Register</button>
-        </form>
-        ${error ? `<p class="error">${error}</p>` : ''}
-        <a href="/">go back</a>`))
+    res.render('register', { error: req.session.error })
 })
 
 app.post('/register', formBodyParser, (req, res) => {
@@ -51,11 +42,16 @@ app.post('/register', formBodyParser, (req, res) => {
 
     try {
         logic.registerUser(name, surname, username, password)
+            .then(() => {
+                req.session.error = null
 
-        error = null
+                res.render('register-confirm', { name })
+            })
+            .catch(({ message }) => {
+                req.session.error = message
 
-        res.send(buildView(`<p>Ok! user ${name} registered.</p>
-                <a href="/">go back</a>`))
+                res.redirect('/register')
+            })
     } catch ({ message }) {
         error = message
 
@@ -64,26 +60,26 @@ app.post('/register', formBodyParser, (req, res) => {
 })
 
 app.get('/login', (req, res) => {
-    res.send(buildView(`<form action="/login" method="POST">
-            <input type="text" name="username" placeholder="username">
-            <input type="password" name="password" placeholder="password">
-            <button type="submit">Login</button>
-        </form>
-        ${error ? `<p class="error">${error}</p>` : ''}
-        <a href="/">go back</a>`))
+    res.render('login', { error: req.session.error })
 })
 
-app.post('/login', [formBodyParser, mySession], (req, res) => {
+app.post('/login', formBodyParser, (req, res) => {
     const { username, password } = req.body
 
     try {
-        const id = logic.authenticateUser(username, password)
+        logic.authenticateUser(username, password)
+            .then(id => {
+                req.session.userId = id
 
-        req.session.userId = id
+                req.session.error = null
 
-        error = null
+                res.redirect('/home')
+            })
+            .catch(({ message }) => {
+                req.session.error = message
 
-        res.redirect('/home')
+                res.redirect('/login')
+            })
     } catch ({ message }) {
         error = message
 
@@ -91,55 +87,92 @@ app.post('/login', [formBodyParser, mySession], (req, res) => {
     }
 })
 
-app.get('/home', mySession, (req, res) => {
+app.get('/home', (req, res) => {
     const id = req.session.userId
-    debugger
+
     if (id) {
-        const user = logic.retrieveUser(id)
+        try {
+            debugger
+            logic.retrieveUser(id)
+                .then(({ name,postits}) => {
+                debugger
+                res.render('home', { name,postits})})
+                .catch(({ message }) => {
+                    debugger
+                    req.session.error = message
 
-        res.send(buildView(`<p>Welcome ${user.name}!</p>
-                                  <form action="/postit" method="POST">
-                                  <input type="text" name="postitText" placeholder="Type text here...">
-                                 <button type="submit">Create postit</button>
-                                 </form>
-                                  ${error ? `<p class="error">${error}</p>` : ''}
+                    res.redirect('/')
+                })
+        } catch ({ message }) {
+            req.session.error = message
 
-
-                        <ul>${user.postits.map(post => `<li class=post>${post.text} <form action="/delete" method="POST">
-                       <button type="submit">delete postit</button>
-                       </form></li>`).join('')} </ul>
-
-                        <a href="/logout">logout</a>`))
+            res.redirect('/')
+        }
     } else res.redirect('/')
 })
 
-app.post('/postit', [formBodyParser, mySession],(req,res)=>{
-    
-    const { postitText } = req.body
+app.get('/new-postit', (req, res) =>{
     const id = req.session.userId
-    const user = logic.retrieveUser(id)
-    try{
-        logic.createPostit(postitText,id)
-        error = null
-        res.send(buildView(`<p>Ok! Postit created</p>
-                           <a href='/home'>Your posts</a>`))
-    }catch ({ message }) {
-        error = message
-        res.redirect('/home')
-    }
+
+    if (id) {
+        try{
+            logic.retrieveUser(id)
+                .then(({ name }) => res.render('new-postit', { name }))
+                .catch(({ message }) => {
+                    req.session.error = message
+
+                    res.redirect('/')
+                })
+        } catch({ message }) {
+            req.session.error = message
+
+            res.redirect('/')
+        }
+    } else res.redirect('/')
 })
 
-app.get('/logout', mySession, (req, res) => {
+app.post('/new-postit', formBodyParser, (req, res) => {
+    
+    const { text } = req.body
+    const id = req.session.userId
+    if(id){
+        try{
+            logic.newPostit(id, text)
+
+            res.redirect('/home')
+
+        } catch({ message }) {
+            req.session.error = message
+
+            res.redirect('/')
+        }
+    } else res.redirect('/')
+
+})
+
+app.post('/delete-postit',formBodyParser,(req,res)=>{
+    
+    const {postitId} = req.body
+    const id = req.session.userId
+    if(id){
+        try{
+            logic.deletePostit(postitId, id)
+                
+            res.redirect('/home')
+
+        } catch({ message }) {
+            req.session.error = message
+
+            res.redirect('/')
+        }
+    } else res.redirect('/')
+
+})
+
+app.get('/logout', (req, res) => {
     req.session.userId = null
 
     res.redirect('/')
 })
 
-app.get('/users', (req, res) => {
-    res.send(buildView(`<ul>
-            ${logic._users.map(user => `<li>${user.id} ${user.name} ${user.surname}</li>`).join('')}
-        </ul>
-        <a href="/">go back</a>`))
-})
-
-app.listen(port, () => console.log(`Server up and running on port ${port}`))
+app.listen(port, () => console.log(`Server ${package.version} up and running on port ${port}`))
--- staff/henry-novoa/postit-web-app/logic.js
+++ staff/henry-novoa/postit-web-app/logic.js
@@ -1,6 +1,5 @@
 const { User, Postit } = require('./data')
 
-
 const logic = {
     registerUser(name, surname, username, password) {
         if (typeof name !== 'string') throw TypeError(`${name} is not a string`)
@@ -13,13 +12,14 @@ const logic = {
         if (!username.trim()) throw Error('username is empty or blank')
         if (!password.trim()) throw Error('password is empty or blank')
 
-        let user = User.findByUsername(username)
+        return User.findByUsername(username)
+            .then(user => {
+                if (user) throw Error(`username ${username} already registered`)
 
-        if (user) throw Error(`username ${username} already registered`)
+                user = new User({ name, surname, username, password })
 
-        user = new User(name, surname, username, password)
-
-        user.save()
+                return user.save()
+            })
     },
 
     authenticateUser(username, password) {
@@ -29,135 +29,61 @@ const logic = {
         if (!username.trim()) throw Error('username is empty or blank')
         if (!password.trim()) throw Error('password is empty or blank')
 
-        const user = User.findByUsername(username)
+        return User.findByUsername(username)
+            .then(user => {
+                if (!user || user.password !== password) throw Error('invalid username or password')
 
-        if (!user || user.password !== password) throw Error('invalid username or password')
-
-        return user.id
+                return user.id
+            })
     },
 
     retrieveUser(id) {
         if (typeof id !== 'number') throw TypeError(`${id} is not a number`)
 
-        const user = User.findById(id)
-        debugger
-        if (!user) throw Error(`user with id ${id} not found`)
+        return User.findById(id)
+            .then(user => {
+                if (!user) throw Error(`user with id ${id} not found`)
+                
+                const _user = user.toObject()
 
-        const _user = new User(
-            user.name,
-            user.surname,
-            user.username,
-        )
-        _user.postits = user.postits
-        _user.id = user.id
+                _user.id = id
 
-        delete _user.password
+                delete _user.password
 
-        return _user
+                return _user
+            })
     },
-    createPostit(text, userId) {
+
+    newPostit(id,text){
+
+        if (typeof id !== 'number') throw TypeError(`${id} is not a number`)
         if (typeof text !== 'string') throw TypeError(`${text} is not a string`)
 
         if (!text.trim()) throw Error('text is empty or blank')
 
-        if (typeof userId !== 'number') throw new TypeError(`${userId} is not a number`)
-
-      
         const postit = new Postit(text)
 
-        let _user = User.findById(userId)
+        return User.findById(id)
+            .then(user => {
+                if (!user) throw Error(`user with id ${id} not found`)
 
-        let user = new User(_user.name,_user.surname,_user.username,_user.password)
+                user.savePostit(postit)
+
+            })
+    },
+
+    deletePostit(postitId,id){
         debugger
-        user.id = _user.id
-        user.savePostit(postit)
-        //this._postits.push(new Postit(text))
+        return User.findById(id)
+            .then(user => {
+                if (!user) throw Error(`user with id ${id} not found`)
+                
+                user.killPostit(postitId)
 
-        
-    },
-
-    listPostitsByUser(userId, token) {
-        debugger
-        if (typeof userId !== 'string') throw new TypeError(`${userId} is not a string`)
-
-        if (!userId.trim()) throw Error('userId is empty or blank')
-
-        if (typeof token !== 'string') throw TypeError(`${token} is not a string`)
-
-        if (!token.trim()) throw Error('token is empty or blank')
-
-        return fetch(`https://skylabcoders.herokuapp.com/api/user/${userId}`, {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        })
-            .then(res => res.json())
-            .then(res => {
-                if (res.error) throw Error(res.error)
-
-                return this._postits = res.data.postits || []
             })
-    },
 
-    deletePostit(id, userId, token) {
-        if (typeof id !== 'number') throw new TypeError(`${id} is not a number`)
-
-        if (typeof userId !== 'string') throw new TypeError(`${userId} is not a string`)
-
-        if (!userId.trim()) throw Error('userId is empty or blank')
-
-        if (typeof token !== 'string') throw TypeError(`${token} is not a string`)
-
-        if (!token.trim()) throw Error('token is empty or blank')
-
-        this._postits = this._postits.filter(postit => postit.id !== id)
-
-        return fetch(`https://skylabcoders.herokuapp.com/api/user/${userId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({ postits: this._postits })
-        })
-            .then(res => res.json())
-            .then(res => {
-                if (res.error) throw Error(res.error)
-            })
-    },
-
-    updatePostit(id, text, userId,token) {
-        if (typeof id !== 'number') throw new TypeError(`${id} is not a number`)
-
-        if (typeof text !== 'string') throw TypeError(`${text} is not a string`)
-
-        if (!text.trim()) throw Error('text is empty or blank')
-        
-        let posit = this._postits.find(text => text.id === id)
-
-        let blabla = posit.text
-
-        let index = this._postits.findIndex(text => text.id === id )
-
-        this._postits[index].text = blabla
-        
-
-
-        return fetch(`https://skylabcoders.herokuapp.com/api/user/${userId}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json; charset=utf-8',
-                'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify({ postits: this._postits })
-        })
-            .then(res => res.json())
-            .then(res => {
-                if (res.error) throw Error(res.error)
-            })
-      
     }
+
 }
 
 module.exports = logic
