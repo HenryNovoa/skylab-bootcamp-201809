5a5f3467c91d9ea0ed53c13e62d4ebe27e47804a
henry <henry.r.novoa@gmail.com>
2018-11-19 12:42:16 +0100

    revert from develop

--- staff/henry-novoa/handyman-project/handyman-api/data/index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const { Job, User } = require('./schemas')
-
-module.exports = {
-    Postit: mongoose.model('Job', Job),
-    User: mongoose.model('User', User)
-}
